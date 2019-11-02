047a0ae635cf3e99c4bf93a1497ad25320c389d4
bcudina <bozidar.cudina@gmail.com>
2019-11-02 21:50:38 +0100

    promjene 02.10.2019.god

--- src/components/skupa.jsx
+++ src/components/skupa.jsx
@@ -2,14 +2,22 @@ import React, { Component } from "react";
 import Counter from "./counter";
 
 class Skupa extends Component {
-  state = {};
+  state = {
+    counters: [
+      { id: 1, value: 4 },
+      { id: 2, value: 2 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 }
+    ]
+  };
   render() {
     return (
       <div>
-        <Counter />
-        <Counter />
-        <Counter />
-        <Counter />
+        {this.state.counters.map(brojevi => (
+          <Counter key={brojevi.id} value={brojevi.value}>
+            {/*<h4>nesto # {brojevi.id}</h4>*/}
+          </Counter>
+        ))}
       </div>
     );
   }
